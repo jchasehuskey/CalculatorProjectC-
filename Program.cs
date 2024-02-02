03126b5ff97533bab16201f260484c88b43b97ee
jchasehuskey <jchase.huskey@gmail.com>
2024-02-02 07:53:58 -0700

    adding console print to direct user

--- Program.cs
+++ Program.cs
@@ -1,91 +1,79 @@
-﻿
-Console.WriteLine("Hello!");
-Console.WriteLine("Input the first number:");
+﻿System.Console.WriteLine("hello");
+System.Console.WriteLine("Input the first number");
 var firstNumber = Console.ReadLine();
-int actualFirstNumber = int.Parse(firstNumber);
+var actualNumber = int.Parse(firstNumber);
 
-Console.WriteLine("Input the second number:");
+System.Console.WriteLine("Input the second number");
 var secondNumber = Console.ReadLine();
-int actualSecondNumber = int.Parse(secondNumber);
+var actualSecondnumber = int.Parse(secondNumber);
 
-Console.WriteLine("What do you want to do with those numbers?");
+System.Console.WriteLine("Which operation should be executed?");
+System.Console.WriteLine("[A]dd numbers");
+System.Console.WriteLine("[S]ubtract numbers");
+System.Console.WriteLine("[M]uliply numbers");
+var choice = Console.Readline();
 
-Console.WriteLine("[A]dd");
-Console.WriteLine("[S]ubtract");
-Console.WriteLine("[M]ultiply");
 
-string userChoice = Console.ReadLine();
 
-// int result = CheckChoice(userChoice);
-// CheckResult(result);
 
-// void displayResult = CheckResult(result);
+System.Console.WriteLine("Press any key to close.");
+Console.ReadKey();
 
-// void CheckResult(int result)
+
+
+
+
+
+// Console.WriteLine("Hello!");
+// Console.WriteLine("Input the first number:");
+// var firstNumber = Console.ReadLine();
+// int actualFirstNumber = int.Parse(firstNumber);
+
+// Console.WriteLine("Input the second number:");
+// var secondNumber = Console.ReadLine();
+// int actualSecondNumber = int.Parse(secondNumber);
+
+// Console.WriteLine("What do you want to do with those numbers?");
+
+// Console.WriteLine("[A]dd");
+// Console.WriteLine("[S]ubtract");
+// Console.WriteLine("[M]ultiply");
+
+// string userChoice = Console.ReadLine();
+
+
+
+// int result;
+// string operationString = "";
+
+// if (userChoice == "A" || userChoice == "a")
 // {
-//     if (result == 0)
-//     {
-//         System.Console.WriteLine("Invalid option");
-//         Console.WriteLine("Press any key to close");
-//         Console.ReadKey();
-//     }
-//     else
-//         System.Console.WriteLine(result);
+//     result = actualFirstNumber
+//     + actualSecondNumber;
+//     operationString = $"{actualFirstNumber} + {actualSecondNumber} = {result}";
+// }
+// else if (userChoice == "S" || userChoice == "s")
+// {
+//     result = actualFirstNumber
+//     - actualSecondNumber;
+//     operationString = $"{actualFirstNumber} - {actualSecondNumber} = {result}";
+// }
+// else if (userChoice == "M" || userChoice == "m")
+// {
+//     result = actualFirstNumber
+//     * actualSecondNumber;
+//     operationString = $"{actualFirstNumber} * {actualSecondNumber} = {result}";
+// }
+// else
+// {
+//     System.Console.WriteLine("Invalid option");
 //     Console.WriteLine("Press any key to close");
 //     Console.ReadKey();
 // }
 
-int result;
-string operationString = "";
-
-if (userChoice == "A" || userChoice == "a")
-{
-    result = actualFirstNumber
-    + actualSecondNumber;
-    operationString = $"{actualFirstNumber} + {actualSecondNumber} = {result}";
-}
-else if (userChoice == "S" || userChoice == "s")
-{
-    result = actualFirstNumber
-    - actualSecondNumber;
-    operationString = $"{actualFirstNumber} - {actualSecondNumber} = {result}";
-}
-else if (userChoice == "M" || userChoice == "m")
-{
-    result = actualFirstNumber
-    * actualSecondNumber;
-    operationString = $"{actualFirstNumber} * {actualSecondNumber} = {result}";
-}
-else
-{
-    System.Console.WriteLine("Invalid option");
-    Console.WriteLine("Press any key to close");
-    Console.ReadKey();
-}
-
-System.Console.WriteLine(operationString);
-Console.WriteLine("Press any key to close");
-Console.ReadKey();
-
-// int CheckChoice(string userChoice)
-// {
-//     if (userChoice == "A" || userChoice == "a")
-//     {
-//         return (actualFirstNumber + actualSecondNumber);
-//     }
-//     else if (userChoice == "S" || userChoice == "s")
-//     {
-//         return (actualFirstNumber - actualSecondNumber);
-//     }
-//     else if (userChoice == "M" || userChoice == "m")
-//     {
-//         return (actualFirstNumber * actualSecondNumber);
-//     }
-//     else
-//     {
-//         return 0;
-//     }
-// }
-
+// System.Console.WriteLine(operationString);
 // Console.WriteLine("Press any key to close");
 // Console.ReadKey();
+
+
+
